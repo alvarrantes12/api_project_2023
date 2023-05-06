e5f10fe9b590654f33cd42c9e45a5f926e793c33
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2023-05-06 14:39:00 -0600

    KJAM-C10724 Directors crud done

---
+++ spec/models/director_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Director, type: :model do
+  subject {
+    described_class.new(first_name: "Director",
+                      last_name: "Director 1")
+  }
+
+  it "valid with valid attributes" do 
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a first_name with less than 3 letters" do
+    subject.first_name = "Jo"
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with a last_name with more than 10 letters" do
+    subject.last_name = "Dasdjasdhasdasda"
+    expect(subject).to_not be_valid
+  end
+end
