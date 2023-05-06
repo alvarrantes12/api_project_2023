95342b649913de06b672fe2e2b07a6f49788a01b
IsaacVG11 <isaac.villalobosg@gmail.com>
2023-05-06 10:51:19 -0600

    IVG-C18512 Directors CRUD done

---
+++ app/models/director.rb
@@ -0,0 +1,6 @@
+class Director < ApplicationRecord
+
+    validates :first_name, presence: true, length: {minimum: 3} 
+    validates :last_name, presence: true, length: {minimum: 3, maximum: 10} 
+
+end
