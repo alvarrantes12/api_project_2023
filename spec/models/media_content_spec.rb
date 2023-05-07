23fc975e7d1ee3037ad190c50248f7e46b2c94f9
jesus <jeadroar05@gmail.com>
2023-05-07 13:03:50 -0600

    JRA-C16536 API Class Done

---
+++ spec/models/media_content_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe MediaContent, type: :model do
+  subject { 
+      described_class.new(name: "Movie",
+                          director: Director.create(first_name: "John", last_name: "Black"))
+  } 
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+end
