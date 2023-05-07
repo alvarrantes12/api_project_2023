0e88b2129b6d8fd1027f78c1fc341cb064fbdbca
davidrorez <davidrorez@gmail.com>
2023-05-06 18:44:38 -0600

    DRR-C06889 API done

---
+++ spec/models/media_content_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe MediaContent, type: :model do
+  subject {
+    described_class.new(name: "TestName",
+                        director: Director.create(first_name: "Jhon,", last_name: "Belford"))
+  }
+
+  it "valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+end
