a58ec6dcfc0126dfbd2a52aa798cd3b311e9feaf
kodruck <jonathan.salazar@ucrso.info>
2023-05-07 19:03:53 -0600

    JON-B97126 Directors crud done

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,47 @@
+class DirectorsController < ApplicationController
+  before_action :set_director, only: %i[ show edit update destroy ]
+
+  def index
+    @directors = Director.all
+  end
+
+  def show; end
+
+  def new
+    @director = Director.new
+  end
+
+  def edit; end
+
+  def create
+    @director = Director.new(director_params)
+
+      if @director.save
+        redirect_to director_url(@director), notice: t('activerecord.modules.director.one')+" "+t('messages.success.successfully_created') 
+      else
+        render :new, status: :unprocessable_entity 
+      end
+  end
+
+  def update
+      if @director.update(director_params)
+        redirect_to director_url(@director), notice: t('activerecord.modules.director.one')+" "+t('messages.success.successfully_updated')  
+      else
+        render :edit, status: :unprocessable_entity 
+      end
+  end
+
+  def destroy
+    @director.destroy
+      redirect_to directors_url, notice: t('activerecord.modules.director.one')+" "+t('messages.success.successfully_destroy')  
+  end
+
+  private
+    def set_director
+      @director = Director.find(params[:id])
+    end
+
+    def director_params
+      params.require(:director).permit(:first_name, :last_name)
+    end
+end
